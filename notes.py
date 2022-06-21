892880f3da1601851aa826ff3ee4f59767b079b6
Aamir Kalimi <codekalimi@gmail.com>
2022-06-21 22:22:28 +0530

    add notes for two different array related problems

---
+++ notes.py
@@ -0,0 +1,8 @@
+"""
+if you find two different array
+please make sure with two pointer
+
+so take two pointer variable
+loop through the two pointer by p1 < len(firstArray) and p2 < len(secondArray)
+based on the condition increase the pointer
+"""
