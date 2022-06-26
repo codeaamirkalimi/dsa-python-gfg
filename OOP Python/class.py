f11c53e187411bb3ef9b1d76c04a6dc1cee747a8
Aamir Kalimi <aamir_k@ormae.com>
2022-06-26 10:48:25 +0530

    create class and intialize properties

---
+++ OOP Python/class.py
@@ -0,0 +1,10 @@
+class Employee:
+    ID = "AK001"
+    salary = 91000
+    department = "Backend Developer"
+
+
+employee = Employee()
+print("ID {ID}".format(ID=employee.ID))
+print("Salary {salary}".format(salary=employee.salary))
+print("Department {department}".format(department=employee.department))
